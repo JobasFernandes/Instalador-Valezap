78ab502c709e3d749c07dc03ef2c15abf86147df
JSFernandes <joseph.automatus@gmail.com>
2023-03-26 23:22:42 -0300

    Update manifest.sh

--- lib/manifest.sh
+++ lib/manifest.sh
@@ -2,6 +2,6 @@
 
 source "${PROJECT_ROOT}"/lib/_backend.sh
 source "${PROJECT_ROOT}"/lib/_frontend.sh
-source "${PROJECT_ROOT}"/lib/_UPDATE.sh
+source "${PROJECT_ROOT}"/lib/UPDATE.sh
 source "${PROJECT_ROOT}"/lib/_system.sh
 source "${PROJECT_ROOT}"/lib/_inquiry.sh
